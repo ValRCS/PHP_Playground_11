0066fb0d40e4c08a207e4447cb96bd219ef84aa3
Valdis Saules <valdis.s.coding@gmail.com>
2019-12-05 11:57:15 +0200

    connection to DB via PDO

--- config/config.php
+++ config/config.php
@@ -1,3 +1,8 @@
 <?php
 const MAX = 11;
 const PAGETITLE = "Most awesome page";
+
+$SERVER = "localhost";
+const USER = "root";
+const PW = "";
+$DB = "music12";
--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 require_once '../config/config.php';
-echo "New index.php";
+
+try {
+    //if we needed to set port it would come after $SERVER;port=3307;dbname=$DB
+    $conn = new PDO("mysql:host=$SERVER;dbname=$DB", USER, PW);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
